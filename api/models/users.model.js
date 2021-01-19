1eaa1f98f7c31893383a59011d80c4b6fd3887c6
Norber <nox_nox_@hotmail.com>
2021-01-19 21:31:47 +0000

    Add template files.

---
+++ api/models/users.model.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: [true, 'Name is required'] },
+  address: {
+    type: String,
+    required: [true, 'Address is required'],
+    maxlength: 128
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    validate: {
+      validator (value) {
+        return /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/.test(value)
+      }
+    },
+    unique: [true, 'This email is registered']
+  },
+  password: { type: String, required: true },
+  role: {
+    type: String,
+    enum: ['admin', 'clinic'],
+    required: false,
+    default: 'clinic'
+  },
+  telephone: { type: Number, required: [true, 'Telephone number is required'] },
+  contactPerson: { type: String, maxLength: 128 },
+  createdAt: { type: Number, default: Date.now() // Get a timestamp :) }
+})
+
+const userModel = mongoose.model('user', userSchema)
+module.exports = userModel
---
+++ public/js/protect.js
@@ -0,0 +1,10 @@
+if (!localStorage.getItem('token')) {
+  window.location = "http://localhost:3000"
+}
+
+/*
+<head>
+  ...
+  <script src="js/protect.js"></script>
+</head>
+*/
